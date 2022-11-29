79b35018b7422bdf6c087e2802fa403927e5fc70
Daniel Bartolini <djb3149@gmail.com>
2022-11-29 17:36:58 -0500

    added getCharacter functions to js

--- script.js
+++ script.js
@@ -8,32 +8,46 @@ function generatePassword () {
   var isSpecial = confirm("Would you like to use special characters?");
   var isNumber = confirm("Would you like to use numbers?");
   var passLength = prompt("How many characters would you like to use?");
+  var specials = "!#$%&()*+,-./:;<=>?@[]^_`{|}~";
+  var letters = "abcdefghijklmnopqrstuvwxyz";
+  var numbers = "0987654321";
 
   while (passLength < 8 || passLength > 128) {
     passLength = prompt("Must be 8 to 128 characters");
   }
 
-  var possibleSpecial = "!#$%&()*+,-./:;<=>?@[]^_`{|}~";
-  var letters = "abcdefghijklmnopqrstuvwxyz";
-  var numbers = "0987654321";
-
-
   if (isUpperCase) {
-    //
+    var getUppercase = function() {
+      return letters.toUpperCase()[Math.floor(Math.random() * letters.length)];
+    }
+    console.log(getUppercase());
   }
-
+  
   if (isLowerCase) {
-    //use lowercase
+    var getLowercase = function() {
+      return letters[Math.floor(Math.random() * letters.length)]; 
+    }
+    console.log(getLowercase());
   }
 
   if (isSpecial) {
-    //use special characters
+    var getSpecial = function() {
+      return specials[Math.floor(Math.random() * specials.length)];
+    }
+    console.log(getSpecial());
   }
 
   if (isNumber) {
-    //use numbers
+    var getNumber = function() {
+      return numbers[Math.floor(Math.random() * numbers.length)];
+    }
+    console.log(getNumber());
   }
   return "";
+
+  var getPassword = function() {
+    var passwordLength = +passlength.value;
+  }
 }
 
 // Write password to the #password input
