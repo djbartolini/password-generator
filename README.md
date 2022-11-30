4f5769fc8fa90dcd564df5e97ea9f5613116744a
Daniel Bartolini <djb3149@gmail.com>
2022-11-30 17:52:26 -0500

    updated README

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# password-generator
+# Password Generator
+
+The Password Generator project is a website I built to create randomly generated passwords to help provide greater security for users. Password criteria is chosen by the user. At the end, a randomly generated password will be displayed based on the user's criteria.
+
+## Table of Contents
+
+- [Description](#description)
+- [Visuals](#visuals)
+- [Support](#support)
+
+## Description
+
+This password generator was made to help give employees and employers a resource for greater password security. The password generator program is initialized by clicking on the "Generate Password" button. Once initialized, the program will confirm the password's criteria and prompt the user for a length. The user's inputs are then saved and used to generate a random password. All selected character types will be included. If no character types are selected, then no password will be generated and the user can retry.
+
+## Visuals
+
+![Screenshot of the password generator](password-generator-pic.png)
+
+## Support
+
+If you encounter any issues with the password generator, please reach out! My email is daniel.barto@gmail.com. Thank you!
--- script.js
+++ script.js
@@ -3,7 +3,6 @@ var generateBtn = document.querySelector("#generate");
 var specials = "!#$%&()*+,-./:;<=>?@[]^_`{|}~";
 var letters = "abcdefghijklmnopqrstuvwxyz";
 var numbers = "0987654321";
-// var noPassword = "You must choose at lease 1 character type!";
 
 function getPasswordOptions () {
   // Your code goes here
@@ -29,16 +28,18 @@ function getPasswordOptions () {
   return passwordOptions;
 }
 
+
 var generatePassword = function() {
   var options = getPasswordOptions();
 
-  var passwordPool = ""; // empty strings to concatenate onto as the conditionals below run
-  var possibleCharacters = "";
+  var passwordPool = ""; 
+  var possibleCharacters = ""; // empty strings to concatenate onto as the conditionals below run
   var guaranteedCharacters = "";
-  var noPassword = "No password generated. You must choose at least 1 character type!";
+  var noPassword = "No password generated. You must choose at least 1 character type! \nYou may retry if you like.";
 
 
-  if (!options.isUpperCase && !options.isLowerCase && !options.isSpecial && !options.isNumber) return noPassword; // if user made no 
+  if (!options.isUpperCase && !options.isLowerCase && !options.isSpecial && !options.isNumber) 
+  return noPassword; // if user made no selection, then the noPassword message displays. User can click generate password to try again.
 
   if (options.isUpperCase) {
     possibleCharacters = possibleCharacters.concat(letters.toUpperCase());
@@ -59,7 +60,9 @@ var generatePassword = function() {
     possibleCharacters = possibleCharacters.concat(numbers);
     guaranteedCharacters = guaranteedCharacters.concat(numbers[Math.floor(Math.random() * numbers.length)]);
   }
-
+   
+// possibleCharacters is now concatenated based on the user's inputs
+// guaranteedCharacters will be substrung to passwordPool
 
   for (var i = 0; i < options.passLength; i++) {
   passwordPool = passwordPool.concat(possibleCharacters[Math.floor(Math.random() * possibleCharacters.length)]);
@@ -76,7 +79,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
